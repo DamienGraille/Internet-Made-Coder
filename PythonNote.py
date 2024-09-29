a70ed0c83f3944bacb6d2fa4fd45c0a217a23c02
Damien Graille <damiengraille@gmail.com>
2024-09-29 08:18:38 +0100

    Update 290924
    Assignments introduction: Git & Github Basics

---
+++ PythonNote.py
@@ -0,0 +1,110 @@
+# # Comment out ctrl + /
+# # print ("Hello, World")
+# # Integer
+# a = 2
+# print (a)
+# # Output: 2 
+# # Integer
+# b = 933445
+# print (b)
+# # Output: 933445
+# # Floatint point
+# pi = 3.14
+# print(pi)
+# # Output: 3.14
+# # String
+# c = "A"
+# print(c)
+# # Output: A
+# # String
+# name = "John Doe"
+# print(name)
+# # Output: "John Doe"
+# # Boolean
+# q = True
+# print(q)
+# # Output: True
+# # Empty value or null data type
+# x = None
+# print(x)
+# # Output: None
+# # 0 = x
+
+# # Output: can't assign to literal
+# import keyword
+# print(keyword.kwlist)
+
+# x = True
+# _y = True
+# 9x = False
+# print(9x)
+# $y = False 
+# print($y)
+
+# has_0_in_it = "still valid"
+# print(has_0_in_it)
+
+# x=9
+# y= X+5
+# print(y)
+
+
+# # Integer
+# a = 2
+# print (type(a))
+# # Output: 2 
+# # Integer
+# b = 933445
+# print (type(b))
+# # Output: 933445
+# # Floatint point
+# pi = 3.14
+# print(type(pi))
+# # Output: 3.14
+# # String
+# c = "A"
+# print(type(c))
+# # Output: A
+# # String
+# name = "John Doe"
+# print(type(name))
+# # Output: "John Doe"
+# # Boolean
+# q = True
+# print(type(q))
+# x = None
+# print(type(x))
+# # Output: None
+
+# a_name = "an_object"
+# print(a_name)
+# print(type(a_name))
+
+# a, b, c = 1, 2, 3
+# print(a,b,c)
+# print(type(a))
+# a, b, c = 1, 2
+# a, b = 1, 2, 3
+
+# a = b = c = 1
+# print(a, b, c)
+
+# a = b = c = 1
+# print(a, b, c)
+# b = 2
+# print(a, b, c)
+
+# x = y = [7, 8, 9]
+# y = [13,8,9]
+# print(x)
+# print(y)
+# print(type(x))
+
+x = [1, 2, [3, 4, 5], 6, 7]
+# print (x[1])
+# print (x[2])
+# print (x[3])
+# print (x[4])
+# print (x[5])
+print (x[6])
+print (x[7])
--- hello.py
+++ hello.py
@@ -1,5 +1,21 @@
-print("Damien is new")
-print(3+10)
+# print("Hello, World!")
+# name = input("What is your name?")
+# print(f"Hello, {name}")
 
 
 
+# asknumber1=input("give me 2 numbers")
+# asknumber2=input("and")
+# calc1=int(asknumber1)
+# calc2=int(asknumber2)
+
+# print(f"Numbers are {asknumber1} and {asknumber2}")
+# print(f"Sum {calc1 + calc2}")
+# print(f"Difference {calc1 - calc2}")
+# print(f"Product {calc1 * calc2}")
+# print(f"Quotient {calc1 / calc2}")
+
+askage=input("how old are you?")
+age=int(askage)
+newage= age + 1
+print(f"next year you will be {newage} years old")
